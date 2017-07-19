f74514f08cd60b4e53f9382bb1111c91789e123e
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2017-07-19 12:00:14 -0600

    6 render comment form with jquery

--- src/comments/static/comments/js/scit.comments.js
+++ src/comments/static/comments/js/scit.comments.js
@@ -2,21 +2,54 @@
     var endpoint = 'http://localhost:8000/api/comments/'
     var dataUrl = $('.load-comments').attr('data-url')
 
-    $.ajax({
-        methos: "GET",
-        url: endpoint,
-        data:{
-            url: dataUrl,
-        }, 
-        success: function(data){
-            //console.log(data)
-            $.each(data, function(index, object){
-                $('.load-comments').append("<li>" + object.content + "</li>")
-            })
-        },
-        error: function(data){
-            console.log('error')
-            console.log(data)
-        },
+    getComments(dataUrl)
+    function getComments(requestUrl){
+        $.ajax({
+            methos: "GET",
+            url: endpoint,
+            data:{
+                url: requestUrl,
+            }, 
+            success: function(data){
+                if (data.length > 0){
+                    $.each(data, function(index, object){
+                        $('.load-comments').append("<li>" + object.content + "</li>")
+                    })
+                }
+                var formHtml = generateForm()
+                $(".load-comments").after(formHtml)
+            },
+            error: function(data){
+                console.log('error')
+                console.log(data)
+            },
+        })
+    }
+
+    function generateForm(){
+        var html_ = "<form method='POST' class='comment-form'><textarea name='content'></textarea><input type='submit' value='Comment'></form>"
+        return html_
+    }
+
+    function handleForm(formData) {
+        console.log(formData)
+        $.ajax({
+            url: endpoint + "create/",
+            method: "POST",
+            data: formData,
+            success: function (data) {
+                console.log(data)
+                getComments(dataUrl)
+            } ,
+            error: function(data){
+                console.log("error")
+                console.log(data)
+            }
+        })
+    }
+    $(document).on('submit', '.comment-form', function(e){
+        e.preventDefault()
+        var formData = $(this).serialize()
+        handleForm(formData)
     })
 })
